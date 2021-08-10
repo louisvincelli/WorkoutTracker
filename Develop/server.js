060322a7df9cefbc8c17177a4c016ef5e4f213d4
louisvincelli <79131518+louisvincelli@users.noreply.github.com>
2021-08-10 00:20:36 -0700

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 mongoose.connect(
-    process.env.MONGODB_URI ||'mongodb://localhost/workout', {
+    process.env.MONGODB_URI ||'mongodb://localhost/DB1', {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
